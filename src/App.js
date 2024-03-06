40960148cff360bec3bf9c3750f50aac03f07d20
kyh012412 <kyh0124.12@gmail.com>
2024-03-06 10:51:35 +0900

    시계완성

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import { useState } from 'react';
 function App() {
   const [time,setTime] = useState(1);
 
+  const handleClick = () =>{
+    setTime(time>11?0:time+1);
+  }
+  console.log('업데이트');
+
   return (
     <div>
       <span>현재 시각: {time} 시</span>
-      <button onClick={()=>{
-        setTime(time==23? 0:time+1);
-      }}>Update</button>
+      <button onClick={handleClick}>Update</button>
     </div>
   );
 }
